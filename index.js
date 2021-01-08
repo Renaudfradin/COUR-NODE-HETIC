257abc3afbefac6210e849c6337f75b19891d6c1
Renaud <renaudfradin@gmail.com>
2021-01-08 16:15:53 +0100

    get avec DB + connexion

--- index.js
+++ index.js
@@ -28,6 +28,18 @@ const app = express();
 var Analytics = require('analytics-node');
 var analytics = new Analytics('FNHMiZJOqi3hBERgUFhiG2RHbPCrY7lU');
 
+const knex = require("knex")({
+    client :"pg",
+    connection:{
+        connectionString: 'postgres://qjhlbpyvvhtbhm:973f1429c729d05fa72509654039394cdd5fed3c0c89366920860458e75c6f74@ec2-54-78-127-245.eu-west-1.compute.amazonaws.com:5432/d86ape4d1799q2?sllmode=disable',
+        ssl:{
+            rejectUnauthorized: false,
+        }
+    },
+});
+
+
+
 //middleware peut venir avant ou aprés l'execution du handler
 //Handler /get /post
 
@@ -50,6 +62,25 @@ app.post('/login',(req,res)=>{
     });
     return res.status(201).json({
         hello:'worldpost'
+    });
+});
+
+
+
+app.get('/users', async (req, res) => {
+
+    let users = [];
+    try {
+        users = await knex.select().from('users');
+    } catch (error) {
+        console.log('An error occured: ', error);
+        return res.status(500).json({
+            bad: 'very bad'
+        });
+    }
+
+    return res.status(200).json({
+        data: users,
     });
 });
 
